f209225a209932eadc79c5502ef3f97a9909013a
Dika Wicaksono <dhkwck00@gmail.com>
2023-10-17 15:55:53 +0700

    add findAllCars function in CarController and add findAllCars route

--- controllers/carController.js
+++ controllers/carController.js
@@ -5,7 +5,7 @@ const ApiError = require("../utils/apiError")
 const createCar = async (req, res, next) => {
   const { name, price, category } = req.body
   try {
-    const car = await Car.create({
+    const newCar = await Car.create({
       name,
       price,
       category,
@@ -14,6 +14,24 @@ const createCar = async (req, res, next) => {
     res.status(400).json({
       status: "Success",
       message: "Data berhasil ditambahkan!",
+      data: {
+        newCar,
+      },
+    })
+  } catch (err) {
+    next(new ApiError(err.message, 500))
+  }
+}
+
+const findAllCars = async (req, res, next) => {
+  try {
+    const car = await Car.findAll()
+
+    res.status(400).json({
+      status: "Success",
+      data: {
+        car,
+      },
     })
   } catch (err) {
     next(new ApiError(err.message, 500))
@@ -22,4 +40,5 @@ const createCar = async (req, res, next) => {
 
 module.exports = {
   createCar,
+  findAllCars,
 }
--- routes/carRouter.js
+++ routes/carRouter.js
@@ -2,6 +2,9 @@ const router = require("express").Router()
 
 const carController = require("../controllers/carController")
 
-router.route("/").post(carController.createCar)
+router
+  .route("/")
+  .post(carController.createCar)
+  .get(carController.findAllCars)
 
 module.exports = router
